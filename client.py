42fe37dc94d374c4074bc5f6bbf8b48e6d505228
IdoNaim <idonai@post.bgu.ac.il>
2026-01-10 16:23:13 +0200

    started implemeneting request messages and add try-catches to make it possible to stop the client and server with ctrl+c

--- client.py
+++ client.py
@@ -1,16 +1,50 @@
 from scapy.all import *
 import socket
 
-offer_msg_port = 13122
 
+#------helper function------
+def handle_offer(server_tcp_port, server_name, address):
+    tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #create a tcp socket
+    tcp_client.connect((address[0], server_tcp_port))
+    tcp_client.sendall(request_msg)
+    #tcp_client.sendto(request_msg, (address[0], server_tcp_port))
+#--------------------------------------
+#------------requst message format------------
+magic_cookie = 0xabcddcba
+request_msg_type = 0x3
+number_of_rounds = 1
+team_name = "DaWinnersXDXD"
+#---------------------------
+#------------udp configurations----------
+offer_msg_port = 13122
+offer_msg_type = 0x2
 udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Bind to "0.0.0.0" (all interfaces) and the specific port
 udp_client.bind(("0.0.0.0", offer_msg_port))
+udp_client.settimeout(1.0)
+#----------------------------------------
+#----------forming request message----------
+encoded_name = team_name.encode('utf-8')[:32].ljust(32, b'\x00')   # encode the server name and ensure it is 32 bytes long
+request_msg = struct.pack('!IBB32s', magic_cookie, request_msg_type, number_of_rounds, encoded_name)
+#--------------------------------------------
+
+
+
+
 while True:
-    data, addr =udp_client.recvfrom(39)
-    print(f"{data.decode('utf-8')}")
-
-
-
-
-#udp_client.close()
+    try:
+        data, addr = udp_client.recvfrom(39)
+        msg = struct.unpack('!IBH32s', data)
+        cookie = msg[0]             
+        msg_type = msg[1]
+        server_tcp_port = msg[2]
+        raw_name = msg[3]
+        server_name = raw_name.decode('utf-8').rstrip('\x00')      #name as string
+        if cookie == magic_cookie and msg_type == offer_msg_type:
+            handle_offer(server_tcp_port, server_name, addr)
+    except socket.timeout:
+        continue
+    except KeyboardInterrupt:
+        print("Closing client")
+        udp_client.close()
+        break
--- server.py
+++ server.py
@@ -1,39 +1,80 @@
 import socket
 import time
 import struct
+import threading
 
-# offer message configuration
+
+#----------------helper functions----------
+def broadcast_offer():
+    while True:
+        if not is_playing:
+            udp_server.sendto(offer_message, ('255.255.255.255', client_offer_msg_port ))
+            print("Sent messsage")
+            time.sleep(2)
+
+def handle_request(client_socket):
+    global is_playing
+    data = client_socket.recv(38)
+    msg = struct.unpack('!IBB32s', data)
+    if magic_cookie == msg[0] and request_msg_type == msg[1] and not is_playing:
+        num_rounds = msg[2]
+        team_name = msg[3]
+        print(f"Team {team_name} vs {server_name}!'\n' Let The Game Begin!")
+        handle_game(client_socket, num_rounds)
+
+#--------------------------------------------------------------------        
+#----------offer message configuration------
 magic_cookie =  0xabcddcba
 offer_msg_type = 0x2
-server_tcp_port = 5000
+server_tcp_port = 0                     #starting value so that the OS gives us an unused port
 server_name = "BlackJackinton"
-#-----------------------------
-#other configurations
+#-------------------------------------------
+#-----------------other configurations-----------
 client_offer_msg_port = 13122
-server_udp_port = 0
+server_udp_port = 0                     #starting value so that the OS gives us an unused port
 server_ip ='0.0.0.0'
+is_playing = False
+request_msg_type = 0x3
+#----------------------------------------------
 
-
-# creating UDP server
-# 1. Create a UDP socket (SOCK_DGRAM = UDP)
+#-----------creating UDP server---------------
 udp_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# Enable sending broadcast packets
-udp_server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-# 2. Bind to address
-udp_server.bind(server_ip, server_udp_port)
+udp_server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)    # Enable sending broadcast packets
+udp_server.bind((server_ip, server_udp_port))                       # Bind to address
 #----------------------------------------------------------------
+#----------- creating TCP server---------------
+tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+tcp_server.bind((server_ip, server_tcp_port))
+tcp_server.settimeout(1.0)
+tcp_server.listen()
 
-#creating encoded offer message to send
+_, server_tcp_port = tcp_server.getsockname()
+#-----------------------------------------------
+#------------------creating encoded offer message-------------
 encoded_name = server_name.encode('utf-8')[:32].ljust(32, b'\x00')   # encode the server name and ensure it is 32 bytes long
 offer_message = struct.pack('!IBH32s', magic_cookie, offer_msg_type, server_tcp_port, encoded_name)
-print("UDP Server is waiting for packets...")
+#-------------------------------------------------------------
+
+
+threading.Thread(target=broadcast_offer, daemon=True).start() # start UDP thread that sends offers if not in a game
 
 while True:
-    # # 3. Receive data and the address it came from
-    # data, addr = udp_server.recvfrom(1024)
-    # print(f"Received {data.decode('utf-8')} from {addr}")
-    
-    # 4. Reply (optional)
-    udp_server.sendto(b"Sigma macho", ('255.255.255.255',offer_msg_port ))
-    print("Sent messsage")
-    time.sleep(2)
+    try:
+        client_socket, client_address = tcp_server.accept()
+        #connected to client, stop offers for now
+        is_playing = True
+
+        handle_request(client_socket)
+    except socket.timeout:
+        continue
+    except KeyboardInterrupt:
+        print("Closing server")
+        tcp_server.close()
+        udp_server.close()
+        break
+    except Exception as e:
+        print(f"got an error trying to handle message from client: {e}")
+    finally:
+        is_playing = False
+    #got a message, check if the message is a request msg
